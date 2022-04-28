77a071983535d3752bac8c02e820d9a7d470c582
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-29 00:44:24 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+// const ObjectId = require("mongodb").ObjectId;
+// const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+  try {
+//     await client.connect();
+//     const carsCollection = client.db("carsCollection").collection("cars");
+
+    // Fetch all cars
+    app.get("/cars", async (req, res) => {
+      const query = {};
+      const cars = await carsCollection.find(query).toArray();
+      res.send(cars);
+    });
+
+  } finally {
+    // client.close();
+  }
+}
+run();
+
+app.get("/", (req, res) => {
+  res.send({
+    products: {
+      allProducts: "https://car-management.herokuapp.com/cars",
+      specificProduct: {
+        pattern: "https://car-management.herokuapp.com/cars/{_id}",
+        example:
+          "https://car-management.herokuapp.com/cars/6263a3f86375db50d03bd09a",
+      },
+    },
+    maintainer: "Toufiq Hasan Kiron <kiron@mygsuite.co>",
+    source: "Coming soon....",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
