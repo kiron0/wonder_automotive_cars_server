bc8bdef86c181e04c7519cb1956127522d4760bb
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-29 01:16:57 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-// const ObjectId = require("mongodb").ObjectId;
+const fileUpload = require("express-fileupload");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;
 // const mongoose = require("mongoose");
 
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cars-warehouse.noktp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
   try {
-    //     await client.connect();
-    //     const carsCollection = client.db("carsCollection").collection("cars");
+    await client.connect();
+    const carsCollection = client.db("carsCollection").collection("cars");
 
     // Fetch all cars
     app.get("/cars", async (req, res) => {
@@ -105,5 +111,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Car Warehouse Management System Listening on port ${port}`);
 });
